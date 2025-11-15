ca9e61af870314ae10dc4add581cd8c2edec7fa2
Joshua <joshuacancinobri@gmail.com>
2025-11-14 21:07:40 -0600

    Encuentra una palabra.py

--- Ejercicio7/Encuentra una palabra.py
+++ Ejercicio7/Encuentra una palabra.py
@@ -1,3 +1,19 @@
 #Ejercicio 7 - Joshua
-word = input("Ingresa la palabra que deseas encontrar: ").upper()
-strn = input("Ingresa la cadena en donde deseas buscar: ").upper()
+def verificar_caracteres_ocultos(cadena_buscada, cadena_principal):
+    primera = cadena_buscada.lower()
+    segunda = cadena_principal.lower()
+    indice_inicio_busqueda = 0
+
+    for letra in primera:
+        posicion_encontrada = segunda.find(letra, indice_inicio_busqueda)
+        if posicion_encontrada == -1:
+            return "No"
+        else:
+            indice_inicio_busqueda = posicion_encontrada + 1
+    return "Sí"
+
+cadena_principal_usuario = input("Ingresa la cadena en donde deseas buscar: ")
+cadena_buscada_usuario = input("Ingresa la palabra que deseas encontrar: ")
+resultado = verificar_caracteres_ocultos(cadena_buscada_usuario, cadena_principal_usuario)
+
+print(f"{resultado}")
